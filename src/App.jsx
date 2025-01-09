64efd7b185bc74dd5456a053022b531d414f20b2
zxlee0114 <napoleon.lee0114@gmail.com>
2025-01-09 11:20:03 +0800

    在 vite 環境中使用 bootstrap 函式

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,138 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+import { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import { Modal } from "bootstrap";
 // 建議外部資源先放，內部資源往後放
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const modalRef = useRef(null);
+  const customModal = useRef(null);
+
+  const openModal = () => {
+    customModal.current.show()
+  };
+
+  const closeModal = () => {
+    customModal.current.hide()
+  }
 
   useEffect(() => {
-    (async () =>{
-      const response = await axios.get("https://randomuser.me/api/")
-      console.log(response)
-    })()
-  }, [])
+    (async () => {
+      const response = await axios.get("https://randomuser.me/api/");
+      console.log(response);
+      openModal();
+      setTimeout(() => {
+        closeModal();
+      }, 2000);
+    })();
+  }, []);
+
+  useEffect( () => {
+    console.log(modalRef.current);
+    // const myModal = new bootstrap.Modal(document.getElementById('myModal'), options)
+    customModal.current = new Modal(modalRef.current);
+    // customModal.current.show()
+  }, []);
+  
+  
 
   return (
     <>
+    {/* 透過 JS 撰寫，可參考文件：https://getbootstrap.com/docs/5.3/components/modal/#via-javascript */}
+    <button
+          type="button"
+          className="btn btn-primary"
+          onClick={openModal}
+        >
+          Launch demo modal
+        </button>
+
+        <div
+          className="modal fade"
+          ref={modalRef}
+          tabIndex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="exampleModalLabel">
+                  Modal title
+                </h1>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">...</div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button type="button" className="btn btn-primary">
+                  Save changes
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+    
+    {/* 用 HTML 寫法，在 main.jsx 匯入 bootstrap 即可：即 import "bootstrap"
+    <button
+          type="button"
+          className="btn btn-primary"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+        >
+          Launch demo modal
+        </button>
+
+        <div
+          className="modal fade"
+          id="exampleModal"
+          tabIndex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="exampleModalLabel">
+                  Modal title
+                </h1>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">...</div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button type="button" className="btn btn-primary">
+                  Save changes
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>*/}
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -27,7 +143,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button className='btn btn-primary' onClick={() => setCount((count) => count + 1)}>
+        <button
+          className="btn btn-primary"
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
         </button>
         <p>
@@ -38,7 +157,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './assets/all.scss'
 //import './index.css'
 import App from './App.jsx'
+// import "bootstrap"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
